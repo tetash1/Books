4ef45002e2a762b36d3571d3ab6e8c026c6b2974
firebase firestore <https://github.com/tetash1/Sellbook-Firebase-firestore>
2023-02-09 23:08:56 +0600

    first update

---
+++ app/src/main/java/com/tetst/mybooksgithub/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tetst.mybooksgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.krishna.fileloader.FileLoader;
+import com.krishna.fileloader.listener.FileRequestListener;
+import com.krishna.fileloader.pojo.FileResponse;
+import com.krishna.fileloader.request.FileLoadRequest;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FileLoader.with(this)
+                .load("https://github.com/tetash1/Books/blob/main/class_six_Bangla_.pdf",false) //2nd parameter is optioal, pass true to force load from network
+                .fromDirectory("test4", FileLoader.DIR_INTERNAL)
+                .asFile(new FileRequestListener<File>() {
+                    @Override
+                    public void onLoad(FileLoadRequest request, FileResponse<File> response) {
+                        File loadedFile = response.getBody();
+                        // do something with the file
+                    }
+
+                    @Override
+                    public void onError(FileLoadRequest request, Throwable t) {
+                    }
+                });
+    }
+}
